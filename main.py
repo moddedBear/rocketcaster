81973675a007c3f5284aedfe841c58396ac3b665
Jeremy Payne <jeremypayne26@gmail.com>
2022-02-09 20:09:16 -0700

    Add host launch argument

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from rocketcaster import app
 parser = argparse.ArgumentParser()
 parser.add_argument('--hostname, -H', type=str,
                     help='the hostname of the server', dest='hostname', default=None)
+parser.add_argument('--host', type=str,
+                    help='the local socket to bind to', dest='host', default='0.0.0.0')
 parser.add_argument('--certfile, -c', type=str,
                     help='the path to the server\'s certfile', dest='certfile', default=None)
 parser.add_argument('--keyfile, -k', type=str,
@@ -20,4 +22,5 @@ if args.certfile:
     server.certfile = args.certfile
 if args.keyfile:
     server.keyfile = args.keyfile
+server.host = args.host
 server.run()
